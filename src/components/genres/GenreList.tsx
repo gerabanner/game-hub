36e07d79ab70b25c3421e7a697fc494e301d834c
Gerardo Barragán <g.barragan@tcondesa.com.mx>
2023-09-18 12:31:46 -0600

    fix some error in css game card and add spinner

--- src/components/genres/GenreList.tsx
+++ src/components/genres/GenreList.tsx
@@ -1,18 +1,27 @@
-import { HStack, List, ListItem, Text } from "@chakra-ui/react";
+import { HStack, Image, List, ListItem, Spinner, Text } from "@chakra-ui/react";
 import React from "react";
 import useGenres from "../../hooks/useGenres";
+import getCroppedImageUrl from "../../services/image-url";
 
 const GenreList = () => {
 
     const { data, isLoading, error } = useGenres();
 
+    if(error) return null;
+    if(isLoading) return <Spinner/>;
+
     return (
         <React.Fragment>
-            {error && <Text>{error}</Text>}
-            {isLoading && <Text>Loading...</Text>}
             <HStack>
                 <List>
-                    {data.map(genre => <ListItem>{genre.name}</ListItem>)}
+                    {data.map(genre =>
+                        <ListItem paddingY={'5px'}>
+                            <HStack>
+                                <Image boxSize={'32px'} borderRadius={8} src={getCroppedImageUrl(genre.image_background)} />
+                                <Text fontSize={'lg'}>{genre.name}</Text>
+                            </HStack>
+                        </ListItem>
+                    )}
                 </List>
             </HStack>
         </React.Fragment>
--- src/types/genres/Genre.module.d.ts
+++ src/types/genres/Genre.module.d.ts
@@ -1,4 +1,5 @@
 export type Genre = {
     id: number,
-    name: string
+    name: string,
+    image_background: string
 }
